cdcb3048de867f3d0ae714b7730bcd19de8be3a2
Dax <limeside@gmail.com>
2016-05-16 16:33:24 -0500

    Edit style guide.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
+$('#searchForm').on('submit',function(event){
+  event.preventDefault();
 
+  var searchText = ($('#searchItem').val());
+  $('#searchItem').val('').fadeOut('slow');
+
+  var beerQuery = $.ajax({
+    type:'GET',
+    dataType:'json',
+    url: 'https://api.brewerydb.com/v2/search?q=' + searchText + '&key=44665a51583c7e1afe237d1dfa5c45b9&format=json'
+  });
+
+  beerQuery.done(function(data){
+    console.log(data);
+  });
+});
