95a86b2357192395eb71a770cd814725b9496439
Dax <limeside@gmail.com>
2016-05-16 21:14:06 -0500

    Clean up.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,6 @@
-var BreweryDb = require('brewerydb-node');
-var brewdb = new BreweryDb('44665a51583c7e1afe237d1dfa5c45b9');
-
 $('#searchForm').on('submit',function(event){
   event.preventDefault();
 
   var searchItem = ($('#searchItem').val());
   $('#searchItem').val('').fadeOut('slow');
-
-  brewdb.search.all({q:searchItem}, function(req,res){
-    console.log(res);
-  });
-
 });
