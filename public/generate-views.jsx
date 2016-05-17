fab8d40fa468dced63ad457e9495c47eedc78e4b
Dax <limeside@gmail.com>
2016-05-17 15:38:48 -0500

    Add psuedocode to work on when I finally figure out how to use React. :I

--- public/generate-views.jsx
+++ public/generate-views.jsx
@@ -8,3 +8,26 @@ function generatePreviews(){
 
   //After looping through, render the resultsArray, fade in if possible.
 }
+
+function generateBeerList(){
+  //create a new section class lists
+
+  //create a div with the brand logo
+
+  //create a ul
+
+    //for each beer listed in the brewery data, make a new li
+
+
+
+  //After looping through, render the page, fade in blah blah.
+}
+
+function generateBeerInfo(){
+  //create a new section class lists
+
+  //create a div with label logo, if available
+
+    //look through and determine what to lists
+    //using object notation, go through and make lis for each key value pair
+}
